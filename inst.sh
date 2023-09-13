1e2e4e5199e7e0451b9a2ec04e6a5fa8a1255d12
akeda <akeda2@gmail.com>
2023-09-13 15:43:20 +0200

    documentation

--- inst.sh
+++ inst.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo "Installing eb to /usr/local/bin/eb..."
+#
+# Copies the python script to /usr/local/bin without using pyinstaller
+#
+echo "Copying eb to /usr/local/bin/eb without using pyinstaller:"
 [ -f eb.py ] && sudo cp --remove-destination eb.py /usr/local/bin/eb
