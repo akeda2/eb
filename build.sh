eb42a961eb2bcd2c36e10775f0646c689a3f5b8e
akeda <akeda2@gmail.com>
2023-09-15 11:04:30 +0200

    build script update

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@
 # Uses pyinstaller:
 if [ ! $(command -v pyinstaller) ]; then
 	if [ ! $(command -v pip3)]; then
-		sudo apt install python3-pip
+		echo "pip3 not installed...installing..."
+		sudo apt install python3-pip || echo "FAILED!"
 	fi
-	sudo pip3 install pyinstaller --break-system-packages
+	sudo pip3 install pyinstaller || sudo pip3 install pyinstaller --break-system-packages && echo "Installed pyinstaller"
 else
-	sudo pip3 install --upgrade pyinstaller --break-system-packages
+	sudo pip3 install --upgrade pyinstaller || sudo pip3 install pyinstaller --break-system-packages && "Updated pyinstaller"
 fi
-
-make clean && make
-printf "\n\nRun 'sudo make install', to install to /usr/local/bin\n"
+echo "Running make..."
+make clean && make || echo "FAILED!"
+printf "\n\nRun 'sudo make install', to install to /usr/local/bin\n\n"
