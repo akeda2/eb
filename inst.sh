c04753e64b855bc5ffe139d585f0704fc3643153
akeda2 <akeda2@gmail.com>
2024-09-10 09:49:46 +0200

    python package first test

--- inst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Copies the python script to /usr/local/bin without using pyinstaller
-#
-echo "Copying eb to /usr/local/bin/eb without using pyinstaller:"
-[ -f eb.py ] && sudo cp --remove-destination eb.py /usr/local/bin/eb
